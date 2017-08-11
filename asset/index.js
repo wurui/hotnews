9295ab0494f045f73b1cf387c17651a5d0ff4f1e
wurui <wurui.wr@gmail.com>
2017-08-11 16:50:37 +0800

    first commit

--- asset/index.js
+++ asset/index.js
@@ -1,7 +1,42 @@
-define([],function(){
+define(['mustache','oxjs'],function(Mustache,OX){
+    var tpl,$list;
+    var href_prefix='https://www.openxsl.com/app/git/wurui/'
   return {
     init:function($mod){
+        tpl=$('.J_tpl',$mod).html();
+        $list=$('.J_list',$mod);
+        var restNews=OX.useREST('news/59892417d7372b422cc4564b');
+        restNews.get({
+            filter:'title,origin_id,cmt,time,type',
+            limit:500
+        },function(r){
+//未登录提示
+            if(r && r.length){
+                var data=[];
+                for(var i=0,n;n=r[i++];){
+                    if(n.cmt){
+                        switch (n.type){
+                            case 'img':
+                                n.href=href_prefix+'img?id='+n.origin_id
+                                break
+                            case 'article':
+                                n.href=href_prefix+'detail?id='+n.origin_id
+                            default :
+                                break
+                        }
+                        data.push(n)
+                    }
+                }
+                data.sort(function(a,b){
+                    return a.cmt > b.cmt ?-1:1;
+                });
+                $list.html(Mustache.render(tpl,data.slice(0,20)))
+            }
+            $('.J_time',$mod).html('数据获取时间:'+(new Date).toLocaleString())
 
+        })
+
+        //$.getJSON('https://www.openxsl.com/rest_dev/news/59892417d7372b422cc4564b?callback=?',
     }
   }
 })
--- mod.xsl
+++ mod.xsl
@@ -2,9 +2,29 @@
     <xsl:template match="/root" name="wurui.hotnews">
         <!-- className 'J_OXMod' required  -->
         <div class="J_OXMod oxmod-hotnews" ox-mod="hotnews">
-            <h1>
-                This is mod hotnews;
-            </h1>
+            <h3 class="headline">
+                <span>热评新闻 Top 20</span>
+                <sub>数据来源:news.ifeng.com</sub>
+            </h3>
+            <table cellspacing="0" cellpadding="0" class="J_list">
+            </table>
+            <br/>
+            <div class="J_time datatime"></div>
+            <script type="text/tpl" class="J_tpl"><![CDATA[
+            <thead>
+                <td><big>标题</big></td>
+                <td>发布时间<br/>评论数</td>
+            </thead>
+            <tbody>
+                {{#.}}
+                <tr>
+                    <td class="title"><a href="{{{href}}}">{{title}}</a></td>
+                    <td>
+                        <nobr class="time">{{time}}</nobr><br/>
+                        <nobr class="cmt">{{cmt}}</nobr>
+                    </td>
+                </tr>{{/.}}</tbody>
+            ]]></script>
         </div>
     </xsl:template>
 </xsl:stylesheet>
